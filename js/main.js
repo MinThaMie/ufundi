7066823a67e6f3eec045f953442833506f74e5f4
Nick Schot <nickschot@gmail.com>
2014-02-28 16:04:11 +0100

    Clickable heads now pop open a popup

--- js/main.js
+++ js/main.js
@@ -5,23 +5,54 @@
     
     function heads(){
         $('#klik-hoofden').delegate('div img', 'click', function(){
-            if(!$(this).parent().hasClass('active')){
-                var defaultOffsetLeft = $(this).parent().css('left');
-                $(this).parent().addClass('active').attr('data-default-left', defaultOffsetLeft);
+            var elemClicked = $(this).parent();
+            //close the opened heads (just in case there are more)
+            $('#klik-hoofden').children().each(function(index, elem){
+                if($(elem).hasClass('active') && $(elem)[0] !== elemClicked[0]){
+                    closeHead($(elem));
+                }
+            });
+            
+            //open the clicked head
+            if(!elemClicked.hasClass('active')){
+                var defaultOffsetLeft = elemClicked.css('left');
+                elemClicked.addClass('active').attr('data-default-left', defaultOffsetLeft);
                 
                 var center = Math.floor($('#klik-hoofden').width() / 2);
                 var imgWidth = Math.floor($(this).width() / 2);
                 var offsetLeft = center - imgWidth;
-                $(this).parent().animate({
-                    'left': offsetLeft
-                });
-            } else {
-                $(this).parent().removeClass('active');
-                var offsetLeft = $(this).parent().attr('data-default-left');
-                $(this).parent().animate({
-                    'left': offsetLeft
-                });
+                elemClicked.animate({
+                    'left': offsetLeft,
+                    'z-index': '999'
+                }, 400, 'swing', function(){popHead(elemClicked);});
             }
         });
     };
+    
+    function popHead(elem){
+        var headWidth = Math.floor(elem.children('img').first().width() / 2);
+        var textWidth = Math.floor(elem.children('div.hoofd-tekst').first().outerWidth() / 2);
+        var textOffset = (textWidth - headWidth)*-1 + 1*elem.children('img').first().css('left').slice(0, -2);
+        
+        elem.children('div.hoofd-tekst').first()
+                .css('left', textOffset)//-1*($(this).width() / 2 - headWidth))
+                .hide().show()
+                .animate({'opacity': 1});
+    };
+    
+    function closeHead(elem){
+        elem.removeClass('active');
+        var offsetLeft = elem.attr('data-default-left');
+        if(offsetLeft !== false){
+            elem.children('div.hoofd-tekst').first().animate({'opacity': 0}, 400, 'swing', function(){
+                $(this)
+                    .hide();
+                elem
+                    .css('z-index', '1')
+                    .animate({
+                        'left': offsetLeft
+                    }, 400, 'swing');
+            });
+        }
+    }
 })(jQuery);
